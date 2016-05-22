a432015cc7ba164d09fa269b659bbeaa48e51802
chenchen <280633979@qq.com>
2016-05-22 12:17:30 +0800

    522

--- js/mobile.js
+++ js/mobile.js
@@ -3,33 +3,7 @@ function getEle(ele) {
 }
 var main=getEle("#main");
 
-~function () {
-    var music = document.getElementById("music"), audioFir = document.getElementById("audioFir");
 
-    //->给页面的加载缓冲500MS时间
-    window.setTimeout(function () {
-        audioFir.play();
-
-        //->当音频文件可以播放(出声了)的时候:canplay/canplaythrough
-        audioFir.addEventListener("canplay", function () {
-            music.style.display = "block";
-            music.className = "music musicMove";
-        }, false);
-    }, 500);
-
-    //->移动端使用CLICK存在300MS的延迟
-    music.addEventListener("click", function () {
-        //->当前是暂停的
-        if (audioFir.paused) {
-            audioFir.play();
-            music.className = "music musicMove";
-            return;
-        }
-        //->当前是播放的
-        audioFir.pause();
-        music.className = "music";
-    }, false);
-}();
 
 $(document).on("touchmove", function (e) {
     e.preventDefault();
@@ -92,13 +66,41 @@ $(function () {
             isOk = true;
         }
         //function end(e){
+        //    if(isOk){
         //    $(this).eq(nextIndex).css("webkitTransition","");
         //    $(this).eq(nextIndex).firstElementChild.id="a"+this.index;
         //
         //}
+        //}
     }
 });
+~function () {
+    var music = document.getElementById("music"), audioFir = document.getElementById("audioFir");
 
+    //->给页面的加载缓冲500MS时间
+    window.setTimeout(function () {
+        audioFir.play();
+
+        //->当音频文件可以播放(出声了)的时候:canplay/canplaythrough
+        audioFir.addEventListener("canplay", function () {
+            music.style.display = "block";
+            music.className = "music musicMove";
+        }, false);
+    }, 500);
+
+    //->移动端使用CLICK存在300MS的延迟
+    music.addEventListener("click", function () {
+        //->当前是暂停的
+        if (audioFir.paused) {
+            audioFir.play();
+            music.className = "music musicMove";
+            return;
+        }
+        //->当前是播放的
+        audioFir.pause();
+        music.className = "music";
+    }, false);
+}();
 
 
 
